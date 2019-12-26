81a946f3668421a74de8a194512d037321c4f3a4
bprlab <bprlab@10.200.144.25>
2019-12-26 20:03:43 +0300

    final 6

---
+++ FinalDers6/src/main/java/org/medipol/yazilimaraclari/FinalDers6/FinalDers6Application.java
@@ -0,0 +1,13 @@
+package org.medipol.yazilimaraclari.FinalDers6;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FinalDers6Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FinalDers6Application.class, args);
+	}
+
+}
